71cebd883b13215d10d14118978d6f5dd01379c1
Alex Norton <ajn123@vt.edu>
2014-07-07 01:28:04 -0700

    solved problem 3

--- problem_1.rb
+++ problem_1.rb
@@ -1,6 +1,6 @@
 # If we list all the natural numbers below 10 that are multiples of 3 or 5,
 # we get 3, 5, 6 and 9. The sum of these multiples is 23.
 
-#Find the sum of all the multiples of 3 or 5 below 1000.
-puts (1...1000).select { |elem| (elem % 3).zero? || (elem % 5).zero? }.inject(:+)
+# Find the sum of all the multiples of 3 or 5 below 1000.
+puts (1...1000).select { |num| num % 5 == 0 || num % 3 == 0 }.inject(:+)
 
---
+++ problem_3.rb
@@ -0,0 +1,6 @@
+# The prime factors of 13195 are 5, 7, 13 and 29.
+# What is the largest prime factor of the number 600851475143 ?
+
+require 'prime'
+
+puts 600_851_475_143.prime_division.to_s
