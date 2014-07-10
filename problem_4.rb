4bad11ea6ab558e1cf975b7788c647f09f813ff8
Alex Norton <ajn123@vt.edu>
2014-07-10 00:58:13 -0700

    problem 6 done and confirmed

--- problem_4.rb
+++ problem_4.rb
@@ -1,14 +1,13 @@
 # A palindromic number reads the same both ways.
 # The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
 # Find the largest palindrome made from the product of two 3-digit numbers.
-require 'prime'
+
 
 class Integer
-  def palindrome
-    self.to_s.reverse == self.to_s
+  def palindrome?
+    self.to_s == self.to_s.reverse
   end
 end
 
-puts (100..999).to_a.permutation(2).to_a.map { |arr| arr[0] * arr[1] }.
-                                         select { |num| num.palindrome }.max
-
+puts (100..999).to_a.permutation(2).map{ |arr|  arr[0] * arr[1] }.
+  select { |num| num.palindrome? }.max
---
+++ problem_6.rb
@@ -0,0 +1,14 @@
+# The sum of the squares of the first ten natural numbers is,
+# 1^2 + 2^2 + ... + 10^2 = 385
+# The square of the sum of the first ten natural numbers is,
+# (1 + 2 + ... + 10)^2 = 55^2 = 3025
+# Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
+# Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+
+one_to_one_hundred = (1..100).to_a
+
+sum_of_separate_squares = one_to_one_hundred.map { |num| num ** 2}.inject(:+)
+
+sum_of_summed_squares = one_to_one_hundred.inject(:+) ** 2
+
+puts "difference", sum_of_summed_squares - sum_of_separate_squares
