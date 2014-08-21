df0114bbf4a3a63cddda40c249e82e80b15bec44
A.J. Norton <ajn123@vt.edu>
2014-08-21 19:56:36 -0400

    reformatte poker code

--- problem_54/problem_54.rb
+++ problem_54/problem_54.rb
@@ -3,6 +3,9 @@ class Match
   def initialize(hands = nil)
   end
 
+  # 1 in hand1 wins
+  # 0 if tie
+  # -1 if hand2 wins
   def best_hand(hand1, hand2)
     return hand1.hand_combination <=> hand2.hand_combination
   end
@@ -15,40 +18,45 @@ class Hand
     @pairs = pairify
   end
 
-  def handify(hand)
-    hand.map { |card| Card.new(card) }.sort { |x, y| x.value <=> y.value }
-  end
-
-  def pairify
-    hand.each_with_object(Hash.new(0)) { |o, h| h[o.value] += 1 }
-  end
-
   # Suits are irrelavent for this problem, but can be added to be more realistic
   def hand_combination
     if royal_flush?
       [9, 14, hand[0].suit]
     elsif straight_flush?
       [8, high_card, hand[0].suit]
-    elsif four_pair
-      [7, high_pair(4), 0]
+    elsif pairs_count(1, 4)
+      [7, high, 0]
     elsif full_house
-      [6, high_pair(3), 0]
+      [6, high_cards, 0]
     elsif flush?
       [5, high_cards, hand[0].suit].flatten
     elsif straight?
       [4, high_cards, hand[0].suit].flatten
-    elsif three_pair
+    elsif pairs_count(1, 3)
       [3, high_cards, 0]
-    elsif two_pairs_count(2)
-      [2, high_cards, 0 ].flatten
-    elsif two_pairs_count(1)
+    elsif pairs_count(2)
+      [2, high_cards, 0].flatten
+    elsif pairs_count(1)
       [1, high_cards, 0].flatten
     else
-      [0, high_cards, 0 ].flatten
+      [0, high_cards, 0].flatten
     end
   end
 
   private
+
+  def full_house
+    pairs_count(1, 2) && pairs_count(1, 3)
+  end
+
+  def handify(hand)
+    hand.map { |card| Card.new(card) }.sort { |x, y| x.value <=> y.value }
+  end
+
+  # Count the frequency of each card
+  def pairify
+    hand.each_with_object(Hash.new(0)) { |o, h| h[o.value] += 1 }
+  end
 
   def high_cards
     cards = hand.map{ |card| card.value }.reverse
@@ -63,30 +71,9 @@ class Hand
     straight? && flush?
   end
 
-  def four_pair
-    number_of_pairs(4)
-  end
-
-  def two_pairs_count(num, pair_num = 2)
-    return true if pairs.count { |k, v| v == 2 } == num
+  def pairs_count(num, pair_num = 2)
+    return true if pairs.count { |k, v| v == pair_num } == num
     return false
-  end
-
-  def high_pair(num)
-    high_pair = pairs.invert
-    high_pair[num]
-  end
-
-  def full_house
-    number_of_pairs(3) && number_of_pairs(2)
-  end
-
-  def three_pair
-    number_of_pairs(3)
-  end
-
-  def number_of_pairs(num)
-    pairs.values.include? num
   end
 
   def straight?
@@ -122,7 +109,6 @@ def run_poker
   player1_count = 0
   m = Match.new
   File.foreach('poker.txt') do |file|
-
     a = file.split
     hand1 = Hand.new(a[0..4])
     hand2 = Hand.new(a[5..9])
