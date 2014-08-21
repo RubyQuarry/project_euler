ee6179e3221862ea39aa6910dd623e08cdfb0fc7
A.J. Norton <ajn123@vt.edu>
2014-08-21 18:56:34 -0400

    finished problem 54

--- problem_54/problem_54.rb
+++ problem_54/problem_54.rb
@@ -1,6 +1,10 @@
-class Player
-  def initialize
+class Match
+  attr_reader :hand
+  def initialize(hands = nil)
+  end
 
+  def best_hand(hand1, hand2)
+    return hand1.hand_combination <=> hand2.hand_combination
   end
 end
 
@@ -19,25 +23,36 @@ class Hand
     hand.each_with_object(Hash.new(0)) { |o, h| h[o.value] += 1 }
   end
 
-  def compare(opponent)
+  # Suits are irrelavent for this problem, but can be added to be more realistic
+  def hand_combination
     if royal_flush?
-      return "You win"
+      [9, 14, hand[0].suit]
     elsif straight_flush?
-      if opponent.straight_flush?
-        return high_card(opponent)
-      end
+      [8, high_card, hand[0].suit]
+    elsif four_pair
+      [7, high_pair(4), 0]
+    elsif full_house
+      [6, high_pair(3), 0]
+    elsif flush?
+      [5, high_cards, hand[0].suit].flatten
+    elsif straight?
+      [4, high_cards, hand[0].suit].flatten
+    elsif three_pair
+      [3, high_cards, 0]
+    elsif two_pairs_count(2)
+      [2, high_cards, 0 ].flatten
+    elsif two_pairs_count(1)
+      [1, high_cards, 0].flatten
+    else
+      [0, high_cards, 0 ].flatten
     end
   end
 
-  def high_card(opponent)
-    4.downto(0) do |num|
-      if hand[num].value > opponent.hand[num].value
-        return "You win"
-      end
-      if hand[num].value < opponent.hand[num].value
-        return "Opponent win"
-      end
-    end
+  private
+
+  def high_cards
+    cards = hand.map{ |card| card.value }.reverse
+    cards.sort { |x, y| [cards.count(y), y] <=> [cards.count(x), x] }
   end
 
   def royal_flush?
@@ -52,16 +67,30 @@ class Hand
     number_of_pairs(4)
   end
 
+  def two_pairs_count(num, pair_num = 2)
+    return true if pairs.count { |k, v| v == 2 } == num
+    return false
+  end
+
+  def high_pair(num)
+    high_pair = pairs.invert
+    high_pair[num]
+  end
+
   def full_house
     number_of_pairs(3) && number_of_pairs(2)
   end
 
+  def three_pair
+    number_of_pairs(3)
+  end
+
   def number_of_pairs(num)
-    @pairs.values.include? num
+    pairs.values.include? num
   end
 
   def straight?
-    hand.each_cons(2).all? { |x, y| y.value == x.value + 1 }
+    hand.each_cons(2).all? { |x, y| y.value == x.value + 1 } || high_cards ==  [14, 5, 4 ,3 , 2]
   end
 
   def flush?
@@ -90,11 +119,18 @@ class Card
 end
 
 def run_poker
+  player1_count = 0
+  m = Match.new
   File.foreach('poker.txt') do |file|
+
     a = file.split
     hand1 = Hand.new(a[0..4])
     hand2 = Hand.new(a[5..9])
+    if m.best_hand(hand1, hand2) == 1
+      player1_count += 1
+    end
   end
+  puts player1_count
 end
 
 run_poker
--- problem_54/problem_54_spec.rb
+++ problem_54/problem_54_spec.rb
@@ -3,50 +3,15 @@ require_relative 'problem_54'
 
 describe 'Poker problem' do
 
-  it 'should do something' do
 
-    c = Card.new('5C')
-    expect(c.value).to eq(5)
-    expect(c.suit).to eq(0)
 
-    c = Card.new('QC')
-
-    expect(c.value).to eq(12)
-  end
-
-  it "be a royal flush" do
-    file = "TC JC QC KC AC"
-    a = file.split
-    hand1 = Hand.new(a[0..4])
-    expect(hand1.royal_flush?).to eq(true)
-  end
-
-  it "not be a royal flush" do
-    file = "9C JC QC KC AC"
-    a = file.split
-    hand1 = Hand.new(a[0..4])
-    expect(hand1.royal_flush?).to eq(false)
-  end
-
-  it "be a straight flush" do
-    file = "9C TC JC QC KC"
-    a = file.split
-    hand1 = Hand.new(a[0..4])
-    expect(hand1.straight_flush?).to eq(true)
-  end
-
-  it "be 4 of a kind" do
-    file = "9C 9C 9C 9C KC"
-    a = file.split
-    hand1 = Hand.new(a[0..4])
-    expect(hand1.four_pair).to eq(true)
-  end
-
-  it "high card challenge" do
-    file = "5C 2D 6D AC 9C 7C 5H 8D TD AS"
-    a = file.split
-    hand1 = Hand.new(a[0..4])
-    hand2 = Hand.new(a[5..9])
-    expect(hand1.high_card(hand2)).to eq("You win")
-  end
+    it "should be a tw pair" do
+      file = "AC 2D 3D 4C 5C 7C 5H 8D TD KS"
+      a = file.split
+      hand1 = Hand.new(a[0..4])
+      hand2 = Hand.new(a[5..9])
+      puts hand1.hand_combination.to_s
+      m = Match.new
+      expect(m.best_hand(hand1, hand2)).to eq(1)
+    end
 end
