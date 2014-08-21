4b0b4bf13864f48151318dff8799d50bd2239695
A.J. Norton <ajn123@vt.edu>
2014-08-21 15:12:13 -0400

    flushes and straights implemented

--- problem_54/problem_54.rb
+++ problem_54/problem_54.rb
@@ -2,10 +2,71 @@ class Player
   def initialize
 
   end
+end
 
+class Hand
+  attr_reader :hand, :pairs
+  def initialize(hand)
+    @hand = handify(hand)
+    @pairs = pairify
+  end
 
+  def handify(hand)
+    hand.map { |card| Card.new(card) }.sort { |x, y| x.value <=> y.value }
+  end
 
+  def pairify
+    hand.each_with_object(Hash.new(0)) { |o, h| h[o.value] += 1 }
+  end
 
+  def compare(opponent)
+    if royal_flush?
+      return "You win"
+    elsif straight_flush?
+      if opponent.straight_flush?
+        return high_card(opponent)
+      end
+    end
+  end
+
+  def high_card(opponent)
+    4.downto(0) do |num|
+      if hand[num].value > opponent.hand[num].value
+        return "You win"
+      end
+      if hand[num].value < opponent.hand[num].value
+        return "Opponent win"
+      end
+    end
+  end
+
+  def royal_flush?
+    hand.map { |card| card.value } == (10..14).to_a && flush?
+  end
+
+  def straight_flush?
+    straight? && flush?
+  end
+
+  def four_pair
+    number_of_pairs(4)
+  end
+
+  def full_house
+    number_of_pairs(3) && number_of_pairs(2)
+  end
+
+  def number_of_pairs(num)
+    @pairs.values.include? num
+  end
+
+  def straight?
+    hand.each_cons(2).all? { |x, y| y.value == x.value + 1 }
+  end
+
+  def flush?
+    hand.all? { |card| card.suit == hand[0].suit  }
+  end
 end
 
 class Card
@@ -18,7 +79,7 @@ class Card
 
   def value
     cards = { "2" => 2, "3" => 3, "4" => 4, "5" => 5, "6" => 6, "7" => 7, "8" => 8, "9" => 9,
-              "10" => 10, "J" => 11, "Q" => 12, "K" => 13, "A" => 14 }
+              "T" => 10, "J" => 11, "Q" => 12, "K" => 13, "A" => 14 }
     cards[@value]
   end
 
@@ -27,3 +88,13 @@ class Card
     suits[@suit]
   end
 end
+
+def run_poker
+  File.foreach('poker.txt') do |file|
+    a = file.split
+    hand1 = Hand.new(a[0..4])
+    hand2 = Hand.new(a[5..9])
+  end
+end
+
+run_poker
--- problem_54/problem_54_spec.rb
+++ problem_54/problem_54_spec.rb
@@ -13,4 +13,40 @@ describe 'Poker problem' do
 
     expect(c.value).to eq(12)
   end
+
+  it "be a royal flush" do
+    file = "TC JC QC KC AC"
+    a = file.split
+    hand1 = Hand.new(a[0..4])
+    expect(hand1.royal_flush?).to eq(true)
+  end
+
+  it "not be a royal flush" do
+    file = "9C JC QC KC AC"
+    a = file.split
+    hand1 = Hand.new(a[0..4])
+    expect(hand1.royal_flush?).to eq(false)
+  end
+
+  it "be a straight flush" do
+    file = "9C TC JC QC KC"
+    a = file.split
+    hand1 = Hand.new(a[0..4])
+    expect(hand1.straight_flush?).to eq(true)
+  end
+
+  it "be 4 of a kind" do
+    file = "9C 9C 9C 9C KC"
+    a = file.split
+    hand1 = Hand.new(a[0..4])
+    expect(hand1.four_pair).to eq(true)
+  end
+
+  it "high card challenge" do
+    file = "5C 2D 6D AC 9C 7C 5H 8D TD AS"
+    a = file.split
+    hand1 = Hand.new(a[0..4])
+    hand2 = Hand.new(a[5..9])
+    expect(hand1.high_card(hand2)).to eq("You win")
+  end
 end
