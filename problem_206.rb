71898586873e6ccf6cb7cc5493db221cad5e2645
A.J. Norton <ajn123@vt.edu>
2014-08-22 03:49:28 -0400

    Optimized problem 206

--- problem_206.rb
+++ problem_206.rb
@@ -1,9 +1,9 @@
 # Find the unique positive integer whose square has the form 1_2_3_4_5_6_7_8_9_0,
 # where each “_” is a single digit.
 
-num = 10
+num = 1389026620
 until (num ** 2).to_s =~ /1.2.3.4.5.6.7.8.9.0/
-  num += 10
+  num -= 10
 end
 
 puts num
